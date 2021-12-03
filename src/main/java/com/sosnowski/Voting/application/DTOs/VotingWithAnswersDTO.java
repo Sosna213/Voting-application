fd24dd52709c1b4658f6a5340da6dde6fb02cfc0
Sosna213 <sosnowskimateusz25@gmail.com>
2021-12-03 14:58:24 +0100

    Changing to send date in good format

--- src/main/java/com/sosnowski/Voting/application/DTOs/VotingWithAnswersDTO.java
+++ src/main/java/com/sosnowski/Voting/application/DTOs/VotingWithAnswersDTO.java
@@ -5,11 +5,15 @@ import com.sosnowski.Voting.application.Entity.Voting;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 @Data
 public class VotingWithAnswersDTO implements Serializable {
     Long votingId;
     String votingName;
     String question;
+    Boolean restricted;
+    String endDate;
     List<String> answers;
 }
--- src/main/java/com/sosnowski/Voting/application/Service/VotingService.java
+++ src/main/java/com/sosnowski/Voting/application/Service/VotingService.java
@@ -13,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +31,8 @@ public class VotingService {
         votingToAdd.setVotingName(addVotingDTO.getVotingName());
         votingToAdd.setQuestion(addVotingDTO.getQuestion());
         User userToAddVoting = new User();
+        votingToAdd.setRestricted(addVotingDTO.getRestricted());
+        votingToAdd.setEndDate(addVotingDTO.getEndDate());
         userToAddVoting.setUserId(addVotingDTO.getUserId());
         votingToAdd.setUser(userToAddVoting);
         final Voting addedVoting = votingRepository.save(votingToAdd);
@@ -46,23 +50,34 @@ public class VotingService {
     public VotingWithAnswersDTO getVotingWithAnswers(Long votingId){
         VotingWithAnswersDTO votingWithAnswersDTO = new VotingWithAnswersDTO();
         votingWithAnswersDTO.setVotingId(votingId);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
         votingWithAnswersDTO.setQuestion(votingRepository.getById(votingId).getQuestion());
         votingWithAnswersDTO.setVotingName(votingRepository.getById(votingId).getVotingName());
+        votingWithAnswersDTO.setRestricted(votingRepository.getById(votingId).getRestricted());
+        if(votingRepository.getById(votingId).getEndDate() != null) {
+            String endDate = dateFormat.format(votingRepository.getById(votingId).getEndDate());
+            votingWithAnswersDTO.setEndDate(endDate);
+        }
         List<String> answers = new ArrayList<>();
         answerRepository.findAnswersByVotingVotingId(votingId).forEach(answer -> {
             answers.add(answer.getAnswer());
         });
         votingWithAnswersDTO.setAnswers(answers);
-
         return votingWithAnswersDTO;
     }
     public List<VotingDTO> getVotingByUserId(String username){
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm");
         List<Voting> votingList = votingRepository.findByUserUsername(username);
         List<VotingDTO> votingDTOList = new ArrayList<>();
         votingList.forEach(voting -> {
             VotingDTO votingDTO = new VotingDTO();
             votingDTO.setVotingId(voting.getVotingId());
             votingDTO.setVotingName(voting.getVotingName());
+            votingDTO.setRestricted(voting.getRestricted());
+            if(voting.getEndDate() != null){
+                String endDate = dateFormat.format(voting.getEndDate());
+                votingDTO.setEndDate(endDate);
+            }
             votingDTO.setQuestion(voting.getQuestion());
             votingDTOList.add(votingDTO);
         });
@@ -82,6 +97,8 @@ public class VotingService {
         Voting votingToEdit = votingRepository.getById(editVotingDTO.getVotingId());
         deleteVotingAndAnswers(editVotingDTO.getVotingId());
         votingToEdit.setVotingName(editVotingDTO.getVotingName());
+        votingToEdit.setEndDate(editVotingDTO.getEndDate());
+        votingToEdit.setRestricted(editVotingDTO.getRestricted());
         votingToEdit.setQuestion(editVotingDTO.getQuestion());
         final Voting addedVoting = votingRepository.save(votingToEdit);
         editVotingDTO.getAnswers().forEach(answerDTO -> {
